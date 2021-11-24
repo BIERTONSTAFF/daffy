4d98f08d018cc6f8bcf2e49bfc462da0e2a6be37
Mikhail Matyushev <48426484+BIERTONSTAFF@users.noreply.github.com>
2021-11-24 23:41:07 +0300

    Add files via upload

--- include/asm_f.h
+++ include/asm_f.h
@@ -2,6 +2,7 @@
 #define DAF_ASM_F_H
 #include "ast.h"
 
+char* asm_f_root(ast_t* ast);
 char* asm_f(ast_t* ast);
 
 #endif
--- include/io.h
+++ include/io.h
@@ -2,5 +2,6 @@
 #define DAF_IO_H
 
 char* daf_read_file(const char* filename);
+void daf_write_file(const char* filename, char* buf);
 
 #endif
